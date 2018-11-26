8555ffee2b199da318d46e4370cbb700eb7a32a2
sevenreup <chris7phiri@gmail.com>
2018-11-27 00:11:35 +0200

    Initial commit

---
+++ app/src/main/java/com/edge/seven/re/up/taxi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.edge.seven.re.up.taxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
